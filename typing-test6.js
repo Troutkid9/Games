4c47bee4c6468eebdc9db65ace8e6f504c1ced74
Troutkid9 <91272741+Troutkid9@users.noreply.github.com>
2022-04-15 21:32:30 -0400

    Add files via upload

---
+++ typing-test6.js
@@ -0,0 +1,265 @@
+const passages = [
+  `Darth Nihilus was a Human male who reigned as a Dark Lord of the Sith during the era of strife following the Jedi Civil War. Before becoming a Sith, he lost everything during the Galactic Republic's war against the Mandalorian Neo Crusaders. He survived the activation of the Mass Shadow Generator superweapon during the war's final battle at the planet Malachor V, which surrounded the planet with a destructive spacial phenomenon known as a mass shadow that obliterated almost everything and everyone on and around the planet. The experience of the shadows made him "hunger" for Force energy, and the affliction began to ravage his body. In his pain he became a wound in the Force and was found by a seeker of such things, the Sith Lord Darth Traya, who told him that she could teach him to feed his hunger. He accepted her offer, becoming Traya's apprentice at the Trayus Academy on Malachor V. Over time, he became one of three concurrent Dark Lords of the Sith.
+
+Together, the three formed a triumvirate with Traya at its head and her other apprentice, Darth Sion, as the third member. The trio chose individual names for themselves and Nihilus became the Lord of Hunger. The apprentices grew in strength during their training and eventually overpowered their Master. Sion defeated Traya, while Nihilus sapped her energy, before the two Sith exiled her and combined their powers to sever her ties to the Force. Nihilus's affliction developed to the point where the Dark Lord was forced to call upon the dark side of the Force to encase his spirit within his mask and armor to stay alive. Nihilus and Sion then became the main perpetrators of a Jedi purge, causing the virtual extinction of the Jedi Order. Nihilus was responsible for the devastation of the planet Katarr, killing and absorbing the Force energy of the Jedi at the Conclave on Katarr, along with every other living thing on the world, save one Miraluka woman named Visas Marr. Nihilus took Marr from the surface and made her his Shadow Hand.
+
+One year later, Nihilus sensed a growing presence in the Force, and sent Marr to investigate and destroy it. The presence was a female Jedi, known only as the Exile, who was on a quest to find the remaining Jedi Masters who had survived the Purge. When Marr attempted to assassinate the woman, the Exile defeated her and convinced her to turn to the light side of the Force in the process. Although Nihilus returned to lurking the fringes of known space, the man was eventually tricked by Traya into initiating the Battle of Telos IV in an attempt to absorb a Jedi Academy without any actual Force-sensitives aside from the Jedi Master Atris, the headmaster. At Telos, the Sith Lord met a large fleet comprised of Republic and Mandalorian forces. In the carnage, his flagship Ravager was secretly boarded by a small force consisting of the Exile, Marr, Mandalore the Preserver, and his Mandalorians. The trio successfully killed Nihilus and destroyed the Ravager. Four millenia later, Nihilus' holocron was in the possession of the Dark Lord Darth Krayt, the founder of the One Sith Order and Emperor of his Galactic Empire. During the final battle of the war, which took place on the planet Malachor V, a human female Jedi General gave the order to activate the Mass Shadow Generator. The Generator, a superweapon conceived by the Zabrak tech specialist Bao-Dur, killed almost everyone on the planet's surface and in orbit nearby.
+
+This man was been conceived by darkside aura energies from the superweapon's destruction of the surface of Malachor and in his grief over his losses during the war, he assumed a dark persona, in part as a means of survival. He was trapped on the planet by the artificially-created mass shadows along with the bulk of the two fleets that filled space around the planet—where the Mandalorians had committed all of their forces in a last attempt at defeating the Republic—and suddenly took ill due to the shadows' effects. An emptiness swept over the man's idle body and it soon began to "hunger" intensely. Without intent, he drained the life-force of another survivor. The act was an unpleasant experience for him, but for a brief moment the hunger ceased. The emptiness, however, came back more relentless and intense than before. He indulged in absorbing the energy of other survivors, but the more he fed, the shorter the hunger was appeased and the more relentless it became.
+
+After the Dark Lord of the Sith Darth Malak was killed by the former Sith Lord and redeemed Jedi Revan at the Jedi Civil War's end, the Sith Empire became divided and its followers turned on each other, with many factions rising to stake claim to what little power could be garnered. The man was eventually discovered by the Dark Lord Darth Traya, who had sensed him as a wound in the Force. After locating the man, Traya explained that it was the Force that fueled his hunger and offered to train him at her Sith Academy on the planet to devour entire worlds to help appease the hunger. He followed her to the Trayus Academy and learned the ways of the Sith. The man apprenticed under Traya during this time, along with another prospective Sith. Traya's hungry pupil and her other apprentice each honed different aspects and skills of the dark side of the Force, until they both became Sith Lords. The man took up the name Nihilus, while the second apprentice became known as Darth Sion. The three Sith joined in the creation of a Sith Triumvirate. In addition, the three each took a unique title, with Nihilus electing for the "Lord of Hunger."
+
+Darth Traya perceived Nihilus's hunger for the Force as detrimental to the Sith's goals. Nevertheless, she taught him how to harness that hunger to make him stronger for a time, as she had promised. Nihilus consumed entire planets on several occasions, caring only to appease his hunger. The dark side consumed him more and more each time, causing his hunger and power to grow dramatically. His power eventually grew beyond what Traya could match. Their views on how to destroy the Jedi also became divided, with he and Sion wanting to destroy them outright while Traya desired a more subtle and time consuming approach. This strained the Triumvirate's already fragile alliance to the breaking point. Nihilus and Sion combined forces to challenge Traya, confronting their teacher in the center of her own Academy. Sion and Nihilus entered the Core from two separate doors and closed in on Traya while igniting their lightsabers; Traya followed suit by igniting hers. Nihilus Force-pushed her into a tooth-like feature of the platform, causing the woman to drop her lightsaber as she slumped against it. Meekly, Traya attempted to reach her lightsaber with the Force but failed as Sion slammed her head into the tooth, then beat her to the ground. Sion combined his power with Nihilus to sap Traya's Force energy. In the process, they also cut her off from the Force and cast her out into exile.
+
+With Traya gone, the Sith were left with no defined leadership and her followers fractured once again into many factions, all seeking to take what little remained. Nihilus and Sion, having been the ones who defeated Traya, took control of the fractured Sith. The two united the numerous Dark Jedi who had followed Traya and had vied for her power themselves. The Dark Lords took control of these groups and focused them on a single common goal: the elimination of the Jedi Order, leading to an extensive assassination campaign against the Jedi, although both Sith Lords had different vehicles for achieving this. On the one hand, Sion killed as many Jedi he could find, dying many times along the way, only to be resurrected by his own anger and will to live fueled by the dark side of the Force. On the other, Nihilus traveled the fringes of known space with a large fleet of ships that he had ripped from the mass shadows surrounding Malachor V. He made one such vessel, the Ravager, his flagship, and led his flotilla to the largest deposits of Force energies he could find, whereupon he sated the ever-growing hunger within him and further increased his power. Both Nihilus and Sion were successful in their pursuits and brought the Jedi to the brink of extinction.
+
+Eventually Nihilus became so absorbed by the dark side of the Force and his hunger that his physical body began to erode. Knowing that he would succumb to death if he did not act soon, Nihilus ripped his spirit from his body and encased it in the armor he wore, thus allowing the dark side to consume his useless body. Using the Force, the Sith Lord was able to keep his robes, armor and mask together, giving him some form and allowing him to use his Force powers as well as a lightsaber. Apart from that, however, he no longer had a physical form and became simple, primitive intent.`,
+];
+
+const wordList =
+  passages[Math.floor(Math.random() * passages.length)].split(/\s+/g);
+
+const $$ = document.querySelectorAll.bind(document);
+
+// Add words to word-section
+function addWords() {
+  // Clear existing word-section
+  const wordSection = $$("#word-section")[0];
+  wordSection.innerHTML = "";
+  $$("#typebox")[0].value = "";
+
+  for (let index = 0; index < wordList.length; index++) {
+    const wordSpan = `<span>${wordList[index]}</span>`;
+    wordSection.innerHTML += wordSpan;
+  }
+
+  // Mark first word as current-word
+  wordSection.firstChild.classList.add("current-word");
+}
+
+// Word Colors
+const colorCurrentWord = "#dddddd";
+const colorCorrectWord = "#93C572";
+const colorIncorrectWord = "#e50000";
+
+// Word Count and other data.
+const wordData = {
+  seconds: 60,
+  correct: 0,
+  incorrect: 0,
+  total: 0,
+  typed: 0,
+};
+
+function checkWord(word) {
+  const wlen = word.value.length;
+  const wval = word.value.trim();
+
+  // How much we have of the current word.
+  const current = $$(".current-word")[0];
+  const currentSubstring = current.innerHTML.substring(0, wlen);
+
+  // Check if we have any typing errors and make sure there is a real
+  // word to check https://github.com/anschwa/typing-test/issues/2
+  const noMatch = wval !== currentSubstring;
+  const emptyWords = wval === "" || currentSubstring === "";
+
+  if (noMatch || emptyWords) {
+    current.classList.add("incorrect-word-bg");
+    return false;
+  } else {
+    current.classList.remove("incorrect-word-bg");
+    return true;
+  }
+}
+
+function submitWord(word) {
+  // Update current-word and keep track of correct & incorrect words
+  const current = $$(".current-word")[0];
+
+  if (checkWord(word)) {
+    current.classList.remove("current-word");
+    current.classList.add("correct-word-c");
+    wordData.correct += 1;
+  } else {
+    current.classList.remove("current-word", "incorrect-word-bg");
+    current.classList.add("incorrect-word-c");
+    wordData.incorrect += 1;
+  }
+
+  // Update wordData
+  wordData.total = wordData.correct + wordData.incorrect;
+
+  // Make the next word the new current-word.
+  current.nextSibling.classList.add("current-word");
+}
+
+function clearLine() {
+  // Remove past words once you get to the next line
+  const wordSection = $$("#word-section")[0];
+  const current = $$(".current-word")[0];
+  const previous = current.previousSibling;
+  const children = $$(".correct-word-c, .incorrect-word-c").length;
+
+  // <span>'s on the next line have a greater offsetTop value than
+  // those on the top line. Remove words until the first word on the
+  // second line is the fistChild of word-section.
+  if (current.offsetTop > previous.offsetTop) {
+    for (let i = 0; i < children; i++) {
+      wordSection.removeChild(wordSection.firstChild);
+    }
+  }
+}
+
+let typingTimer = null;
+function isTimer(seconds) {
+  // BUG: page refresh with keyboard triggers onkeyup and starts timer
+  const time = $$("#timer > span")[0].innerHTML;
+  if (time === "0:00") {
+    return false;
+  }
+
+  // Only set timer once
+  if (time === "1:00" && typingTimer === null) {
+    typingTimer = window.setInterval(() => {
+      if (seconds <= 0) {
+        window.clearInterval(typingTimer);
+      } else {
+        seconds -= 1;
+        const timePad = seconds < 10 ? "0" + seconds : seconds; // Zero padded
+
+        $$("#timer > span")[0].innerHTML = `0:${timePad}`;
+      }
+    }, 1000);
+  }
+
+  return true;
+}
+
+function calculateWPM(data) {
+  const { seconds, correct, incorrect, total, typed } = data;
+  const minutes = seconds / 60;
+  const wpm = Math.max(0, Math.ceil((typed / 5 - incorrect) / minutes));
+  const accuracy = Math.ceil((correct / total) * 100);
+
+  const results = `
+<ul id="results">
+  <li>WPM: <span class="wpm-value">${wpm}</span></li>
+  <li>Accuracy: <span class="wpm-value">${accuracy}%</span></li>
+  <li id="results-stats">
+    Total Words: <span>${total}</span> |
+    Correct Words: <span>${correct}</span> |
+    Incorrect Words: <span>${incorrect}</span> |
+    Characters Typed: <span>${typed}</span>
+  </li>
+</ul>
+`;
+
+  $$("#word-section")[0].innerHTML = results;
+
+  // Color-code accuracy
+  const wpmClass = $$("li:nth-child(2) .wpm-value")[0].classList;
+  if (accuracy > 80) {
+    wpmClass.add("correct-word-c");
+  } else {
+    wpmClass.add("incorrect-word-c");
+  }
+
+  //   console.log(wordData);
+}
+
+function typingTest(e) {
+  const SPACE = 32;
+
+  // Get key code of current key pressed.
+  e = e || window.event;
+  const kcode = e.keyCode;
+  const word = $$("#typebox")[0];
+
+  // Check if empty (starts with space)
+  if (word.value.match(/^\s/g)) {
+    word.value = "";
+    return;
+  }
+
+  // Display typing test results when timer runs out.
+  const isGameover = !isTimer(wordData.seconds);
+  if (isGameover) {
+    calculateWPM(wordData);
+    return;
+  }
+
+  // Otherwise, keep score when timer is on.
+  checkWord(word);
+  if (kcode === SPACE) {
+    submitWord(word);
+    clearLine();
+
+    $$("#typebox")[0].value = "";
+  }
+
+  wordData.typed += 1;
+}
+
+function restartTest() {
+  $$("#typebox")[0].value = "";
+  window.location.reload();
+}
+
+/**
+ * Builds the typing test HTML and all
+ * @param id Name of the element to place the typing test inside of
+ */
+function buildTypingTest(id) {
+  // inject the CSS
+  let styles = `#typing-test {margin: 0 auto;}#typing-test > section {padding: 0.5em;margin: 0 auto;}#timer,#restart {margin: 0.2em;line-height: 2.2em;height: 2.2em;}#word-section {width: 86%;font-size: 1.5em;height: 4em;line-height: 2em;border-radius: 0.25em;position: relative;overflow: hidden;}#word-section > span {display: inline-block;margin-left: 0.2em;}#type-section {text-align: center;}#type-section > * {font-size: 1.5em;display: inline-block;border-radius: 0.25em;color: #fff;vertical-align: middle;}#typebox {width: 64%;color: #000;padding: 0.5em;}#timer {width: 4em;background-color: #21557f;margin-right: 0;}#restart {width: 2em;background-color: #437ea1;}#restart > span {display: inline-block;transform: rotate(1.5rad);font-weight: bold;}#restart:hover {background-color: #21557f;}#results {text-align: center;margin: 0 1%;display: block;}#results li {list-style: none;}#results li:first-child {font-size: 1.5em;}#results li:nth-child(2) {font-size: 0.8em;line-height: 1em;}#results #results-stats {font-size: 0.6em;}footer {margin-top: 1em;text-align: center;font-size: 0.8em;color: #000;}footer a {color: #000;text-decoration: none;}footer a:hover {text-decoration: underline;}.magic-box {position: fixed;width: inherit;height: 0.5em;top: 54px;background-color: #fff;}.waiting {text-align: center;line-height: 1.5em;font-size: 3em;}.current-word {background-color: #dddddd;}.correct-word-c {color: #93c572;}.incorrect-word-c {color: #e50000;}.incorrect-word-bg {background-color: #e50000;}`;
+  var styleSheet = document.createElement("style");
+  styleSheet.type = "text/css";
+  styleSheet.innerText = styles;
+  document.head.appendChild(styleSheet);
+
+  // add the HTML
+  let tag = document.getElementById(id);
+  tag.innerHTML = `
+    <div id="typing-test">
+      <section id="word-section">
+        <div class="waiting">&#9203;</div>
+      </section>
+
+      <section id="type-section">
+        <input id="typebox" name="typebox" type="text" tabindex="1" autofocus onkeyup="typingTest(event)" />
+        <div id="timer" class="type-btn"><span>1:00</span></div>
+        <button id="restart" class="type-btn" tabindex="2" onclick="restartTest()">
+          <span id="restart-symbol">&#8635;</span>
+        </button>
+      </section>
+    </div>
+  `;
+  addWords();
+}
+
+/**
+ * Adapted from https://github.com/anschwa/typing-test/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Adam Schwartz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
