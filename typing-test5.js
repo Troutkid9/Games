ddd6306b72c66aea6d8b5b3141afcc681d48e22b
Troutkid9 <91272741+Troutkid9@users.noreply.github.com>
2022-04-15 21:21:24 -0400

    Add files via upload

---
+++ typing-test5.js
@@ -0,0 +1,265 @@
+const passages = [
+  `The Story of Darth Revan: Darth Revan was an extremely influential figure in both the Mandalorian Wars and the Jedi Civil War. The man known as “Revan” was Jedi, Sith and general across his long and extensive career. As a charismatic young Jedi Knight, Revan ignored the Jedi Council’s orders to remain neutral in the Mandalorian Wars and led many Jedi, including his friend Malak, into battle. While pursuing the remnants of the beaten Mandalorian fleet, Revan encountered the Sith Emperor on Dromund Kaas and–also alongside Malak–fell to the dark side. Becoming Darth Revan, he led his followers in a war to conquer the Republic and re-establish a Sith Order, until he was betrayed and nearly killed by Malak. As a result of his injuries, Revan lost his memory and was given a new identity by the Jedi Council. The reborn Revan fought against his old followers, defeating Malak and redeeming himself–but the existence of the true Sith Empire, hidden away in the dark of the galaxy, remained a secret. In the aftermath, as his memory began to return, Revan left the Republic to confront the Sith Emperor–but his plans to destroy the Emperor failed. Revan’s allies were killed and Revan himself was imprisoned, kept alive by unnatural means for three hundred years while the Sith Emperor fed on his strength. However, the link between them went both ways, and Revan was able to subtly keep the Emperor in check. With Revan now free, questions remain–both about the possibility of renewed aggression from the Emperor, and what effect such a long exposure to the Sith Emperor’s mind may have had on Revan himself. Revan, also known as the Revanchist and Darth Revan, was a male human Jedi Knight turned-Sith Lord and Dark Lord of the Sith until, stripped of his true persona, he returned to the crumbling Jedi Order and helped defeat the Sith Empire he had established. He was acknowledged as a very gifted Force-sensitive pupil, he was trained as a Padawan by number of other Jedi Masters and Kreia, both on Coruscant and at the Jedi Enclave on Dantooine.
+  
+  Revan, also known as the Revanchist and Darth Revan, was a male human Jedi Knight turned-Sith Lord and Dark Lord of the Sith until, stripped of his true persona, he returned to the crumbling Jedi Order and helped defeat the Sith Empire he had established. He was acknowledged as a very gifted Force-sensitive pupil, he was trained as a Padawan by number of other Jedi Masters and Kreia, both on Coruscant and at the Jedi Enclave on Dantooine.
+
+  Being very charismatic, by 3,964 BBY, Revan was a Jedi Knight respected enough to amass a notable following that argued for Jedi intervention in the Mandalorian Wars. Though opposed in this by the Jedi Council itself, he could not be dissuaded. Revealing the horror of the Mandalorians' genocide of the Cathar, Revan bolstered his support base and then quickly circumvented the Council, exploiting a technicality that forced it to begrudgingly "sanction" the intervention he desired. With the Council sidelined, the charismatic young Jedi and his closest friend, Malak, were able to recruit and lead a faction of the Jedi Order to war without fear of reprisal against the will of the Jedi Council, made up of Jedi Masters.
+  
+  A talented military tactician and strategist, Revan directed the Galactic Republic to victory after assuming command of its forces in 3,961 BBY, finally ending the conflict the following year with the defeat and execution of Mandalore the Ultimate, the destruction of Malachor V and the disarmament of the Mandalorian Neo-Crusaders, acts which earned him a reputation among the Mandalorians as Revan the Butcher. In the wake of this triumph, Revan, his loyal Jedi followers, and the third of the Republic fleet that remained under his direct control ventured into the Unknown Regions, ostensibly in pursuance of Mandalorian stragglers - and ceased all communications with the known galaxy.
+  
+  In those unknown regions of space, on Dromund Kaas, Revan and Malak came face to face with the long hidden Sith Empire and it's Emperor. Thus, having already embraced the seductive Sith teachings he encountered at the Trayus Academy on Malachor V, Revan's induction into their traditions was finally completed. Bid by his newfound master to acquire the Star Forge - a superweapon of the Infinite Empire, on his behalf, Revan successfully tracked down the last Rakatan Star Maps required to reveal its location. Upon securing the ancient space station, however, he returned to the civilization he had left behind openly as Darth Revan, proclaiming himself to be the Dark Lord of the Sith. Commanding the continued obedience of many veterans of the Mandalorian Wars, and alongside the man who was now his Sith apprentice, Darth Malak, Revan betrayed the Jedi, turned against the Republic, and plunged the known galaxy into the Jedi Civil War.
+  
+  Utilizing the Star Forge to spawn an immense armada, Revan cut a carefully calculated path of conquest through the Outer Rim, building his own powerful Sith Empire to which new converts regularly flocked. Though the Republic staved off total defeat for two years, thanks to the extraordinary battle meditation of the gifted Jedi Bastila Shan, Revan's ultimate triumph seemed inevitable until he and Malak were ensnared by a Jedi trap. As a Republic fleet engaged that of the two ruling Sith, a strike team led by Shan boarded Revan's flagship and battled its way to the Dark Lord. Moments before the Jedi engaged Revan in combat, however, Malak - sensing an opportunity to be rid of both the Jedi and the Master whose power he coveted - had his own vessel open fire on the distracted Sith Lord's bridge. Caught by surprise, Revan was incapacitated by Malak's attack and then captured by the strike team while his traitorous apprentice, believing his Master destroyed, seized control of his Empire.
+  
+  His mind shattered, his memories scattered, and soon thought dead by the galaxy-at-large, Revan's life was preserved by none other than Shan, an act that forged a Force bond between them. Either unable or unwilling to restore their prisoner's true self, but requiring the knowledge buried within his mind, the Jedi Council used the Force to rebuild Revan with a new identity loyal to the Republic. Almost one year later, after Revan, still oblivious to his true past, rescued the captive Shan alongside Carth Onasi and an eclectic band of allies on Taris, the Jedi Council on Dantooine opted to retrain him. Reawakening to his former skills and charged with unearthing the source of Malak's colossal fleet, Revan and his loyal comrades thwarted all obstacles and adversaries as he and Shan were guided by shared "visions", in truth Revan's own memories - to the Star Maps on Tatooine, Kashyyyk, Manaan, and Korriban.
+  
+  In the midst of this odyssey, the group was ensnared by Admiral Saul Karath, commander of the Sith fleet, and imprisoned aboard the Leviathan. Though Revan and his friends escaped, killing Karath along the way, Darth Malak confronted his former Master, revealed his true identity and ultimately captured Shan, the woman Revan had come to love. Continuing in the face of this loss, Revan and his allies successfully discovered the final Star Map and the location of the Star Forge. There, in the Rakata System, Revan rejected the darkness of his past, turned Shan - whom Malak had broken to his will - back to the light and vanquished the reigning Dark Lord, playing his part in the battle that brought the war he had instigated to a close. Two years onward, having driven the warring Sith factions from Korriban, and becoming a Jedi Master, Revan - with more memories returning - left the known galaxy behind on a solitary quest against the true Sith Empire. As of 3,641 BBY, he was thought to have never returned, until now.`,
+];
+
+const wordList =
+  passages[Math.floor(Math.random() * passages.length)].split(/\s+/g);
+
+const $$ = document.querySelectorAll.bind(document);
+
+// Add words to word-section
+function addWords() {
+  // Clear existing word-section
+  const wordSection = $$("#word-section")[0];
+  wordSection.innerHTML = "";
+  $$("#typebox")[0].value = "";
+
+  for (let index = 0; index < wordList.length; index++) {
+    const wordSpan = `<span>${wordList[index]}</span>`;
+    wordSection.innerHTML += wordSpan;
+  }
+
+  // Mark first word as current-word
+  wordSection.firstChild.classList.add("current-word");
+}
+
+// Word Colors
+const colorCurrentWord = "#dddddd";
+const colorCorrectWord = "#93C572";
+const colorIncorrectWord = "#e50000";
+
+// Word Count and other data.
+const wordData = {
+  seconds: 60,
+  correct: 0,
+  incorrect: 0,
+  total: 0,
+  typed: 0,
+};
+
+function checkWord(word) {
+  const wlen = word.value.length;
+  const wval = word.value.trim();
+
+  // How much we have of the current word.
+  const current = $$(".current-word")[0];
+  const currentSubstring = current.innerHTML.substring(0, wlen);
+
+  // Check if we have any typing errors and make sure there is a real
+  // word to check https://github.com/anschwa/typing-test/issues/2
+  const noMatch = wval !== currentSubstring;
+  const emptyWords = wval === "" || currentSubstring === "";
+
+  if (noMatch || emptyWords) {
+    current.classList.add("incorrect-word-bg");
+    return false;
+  } else {
+    current.classList.remove("incorrect-word-bg");
+    return true;
+  }
+}
+
+function submitWord(word) {
+  // Update current-word and keep track of correct & incorrect words
+  const current = $$(".current-word")[0];
+
+  if (checkWord(word)) {
+    current.classList.remove("current-word");
+    current.classList.add("correct-word-c");
+    wordData.correct += 1;
+  } else {
+    current.classList.remove("current-word", "incorrect-word-bg");
+    current.classList.add("incorrect-word-c");
+    wordData.incorrect += 1;
+  }
+
+  // Update wordData
+  wordData.total = wordData.correct + wordData.incorrect;
+
+  // Make the next word the new current-word.
+  current.nextSibling.classList.add("current-word");
+}
+
+function clearLine() {
+  // Remove past words once you get to the next line
+  const wordSection = $$("#word-section")[0];
+  const current = $$(".current-word")[0];
+  const previous = current.previousSibling;
+  const children = $$(".correct-word-c, .incorrect-word-c").length;
+
+  // <span>'s on the next line have a greater offsetTop value than
+  // those on the top line. Remove words until the first word on the
+  // second line is the fistChild of word-section.
+  if (current.offsetTop > previous.offsetTop) {
+    for (let i = 0; i < children; i++) {
+      wordSection.removeChild(wordSection.firstChild);
+    }
+  }
+}
+
+let typingTimer = null;
+function isTimer(seconds) {
+  // BUG: page refresh with keyboard triggers onkeyup and starts timer
+  const time = $$("#timer > span")[0].innerHTML;
+  if (time === "0:00") {
+    return false;
+  }
+
+  // Only set timer once
+  if (time === "1:00" && typingTimer === null) {
+    typingTimer = window.setInterval(() => {
+      if (seconds <= 0) {
+        window.clearInterval(typingTimer);
+      } else {
+        seconds -= 1;
+        const timePad = seconds < 10 ? "0" + seconds : seconds; // Zero padded
+
+        $$("#timer > span")[0].innerHTML = `0:${timePad}`;
+      }
+    }, 1000);
+  }
+
+  return true;
+}
+
+function calculateWPM(data) {
+  const { seconds, correct, incorrect, total, typed } = data;
+  const minutes = seconds / 60;
+  const wpm = Math.max(0, Math.ceil((typed / 5 - incorrect) / minutes));
+  const accuracy = Math.ceil((correct / total) * 100);
+
+  const results = `
+<ul id="results">
+  <li>WPM: <span class="wpm-value">${wpm}</span></li>
+  <li>Accuracy: <span class="wpm-value">${accuracy}%</span></li>
+  <li id="results-stats">
+    Total Words: <span>${total}</span> |
+    Correct Words: <span>${correct}</span> |
+    Incorrect Words: <span>${incorrect}</span> |
+    Characters Typed: <span>${typed}</span>
+  </li>
+</ul>
+`;
+
+  $$("#word-section")[0].innerHTML = results;
+
+  // Color-code accuracy
+  const wpmClass = $$("li:nth-child(2) .wpm-value")[0].classList;
+  if (accuracy > 80) {
+    wpmClass.add("correct-word-c");
+  } else {
+    wpmClass.add("incorrect-word-c");
+  }
+
+  //   console.log(wordData);
+}
+
+function typingTest(e) {
+  const SPACE = 32;
+
+  // Get key code of current key pressed.
+  e = e || window.event;
+  const kcode = e.keyCode;
+  const word = $$("#typebox")[0];
+
+  // Check if empty (starts with space)
+  if (word.value.match(/^\s/g)) {
+    word.value = "";
+    return;
+  }
+
+  // Display typing test results when timer runs out.
+  const isGameover = !isTimer(wordData.seconds);
+  if (isGameover) {
+    calculateWPM(wordData);
+    return;
+  }
+
+  // Otherwise, keep score when timer is on.
+  checkWord(word);
+  if (kcode === SPACE) {
+    submitWord(word);
+    clearLine();
+
+    $$("#typebox")[0].value = "";
+  }
+
+  wordData.typed += 1;
+}
+
+function restartTest() {
+  $$("#typebox")[0].value = "";
+  window.location.reload();
+}
+
+/**
+ * Builds the typing test HTML and all
+ * @param id Name of the element to place the typing test inside of
+ */
+function buildTypingTest(id) {
+  // inject the CSS
+  let styles = `#typing-test {margin: 0 auto;}#typing-test > section {padding: 0.5em;margin: 0 auto;}#timer,#restart {margin: 0.2em;line-height: 2.2em;height: 2.2em;}#word-section {width: 86%;font-size: 1.5em;height: 4em;line-height: 2em;border-radius: 0.25em;position: relative;overflow: hidden;}#word-section > span {display: inline-block;margin-left: 0.2em;}#type-section {text-align: center;}#type-section > * {font-size: 1.5em;display: inline-block;border-radius: 0.25em;color: #fff;vertical-align: middle;}#typebox {width: 64%;color: #000;padding: 0.5em;}#timer {width: 4em;background-color: #21557f;margin-right: 0;}#restart {width: 2em;background-color: #437ea1;}#restart > span {display: inline-block;transform: rotate(1.5rad);font-weight: bold;}#restart:hover {background-color: #21557f;}#results {text-align: center;margin: 0 1%;display: block;}#results li {list-style: none;}#results li:first-child {font-size: 1.5em;}#results li:nth-child(2) {font-size: 0.8em;line-height: 1em;}#results #results-stats {font-size: 0.6em;}footer {margin-top: 1em;text-align: center;font-size: 0.8em;color: #000;}footer a {color: #000;text-decoration: none;}footer a:hover {text-decoration: underline;}.magic-box {position: fixed;width: inherit;height: 0.5em;top: 54px;background-color: #fff;}.waiting {text-align: center;line-height: 1.5em;font-size: 3em;}.current-word {background-color: #dddddd;}.correct-word-c {color: #93c572;}.incorrect-word-c {color: #e50000;}.incorrect-word-bg {background-color: #e50000;}`;
+  var styleSheet = document.createElement("style");
+  styleSheet.type = "text/css";
+  styleSheet.innerText = styles;
+  document.head.appendChild(styleSheet);
+
+  // add the HTML
+  let tag = document.getElementById(id);
+  tag.innerHTML = `
+    <div id="typing-test">
+      <section id="word-section">
+        <div class="waiting">&#9203;</div>
+      </section>
+
+      <section id="type-section">
+        <input id="typebox" name="typebox" type="text" tabindex="1" autofocus onkeyup="typingTest(event)" />
+        <div id="timer" class="type-btn"><span>1:00</span></div>
+        <button id="restart" class="type-btn" tabindex="2" onclick="restartTest()">
+          <span id="restart-symbol">&#8635;</span>
+        </button>
+      </section>
+    </div>
+  `;
+  addWords();
+}
+
+/**
+ * Adapted from https://github.com/anschwa/typing-test/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Adam Schwartz
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
